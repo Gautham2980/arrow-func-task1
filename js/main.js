7764997c6f33167328454fb9fe22d59df89bb730
Gautham2980 <gautham4816@gmail.com>
2023-10-21 11:27:22 +0530

    arrowtask1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var a=prompt("enter a number");
+document.write("The given number ="+a+" ");
+
+let res=(a)=>{
+    return a**2;
+}
+
+// let result=res(a);
+document.write("The square of the number="+res(a))
+
